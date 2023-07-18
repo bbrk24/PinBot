a51117eabe8ec5d6ccd7b5988f8e087c3174b5a9
bbrk24 <bbrk24@gmail.com>
2023-07-17 23:50:49 -0400

    Add help command

---
+++ src/Commands/HelpCommand.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Interactions;
+
+namespace PinBot.Commands;
+
+public class HelpCommand : InteractionModuleBase
+{
+
+    [SlashCommand("help", "Explain what the bot does", true)]
+    public async Task HelpAsync()
+    {
+        await Context.Interaction.RespondAsync(@"This bot helps you manage pins in your threads.
+
+React to a message with 📌 to pin it, or with 🚫 to remove a pinned message.
+The bot only responds to reactions posted by the thread's OP. Anyone else can pile on 📌, but it won't do anything. It also won't pin messages in non-thread channels.",
+            ephemeral: true
+        );
+    }
+}
